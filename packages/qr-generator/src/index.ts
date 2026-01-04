a5c2d5f0e4ecc3481b476827cd7a66a0d279fc8f
Thomas Lee <thomas930131@gmail.com>
2026-01-03 20:51:23 -0800

    Thomas/restructure (#7)
    * move deployable services under the apps folder
    
    * put qr-generator into packages
    
    * rename web to app
    
    * ran pnpm i

---
+++ packages/qr-generator/src/index.ts
@@ -0,0 +1,7 @@
+export { buildErc681Request, parseErc681Request } from "./erc681.js";
+export type { BuildErc681RequestParams, ParsedErc681Request, Erc681SchemaPrefix } from "./erc681.js";
+
+export { generateQr } from "./qr.js";
+export type { GenerateQrParams, QrOutputFormat } from "./qr.js";
+
+
