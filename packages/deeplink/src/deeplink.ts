2b9127705bacaefb8e32d416d71e89c90fe6b4f3
Thomas Lee <thomas930131@gmail.com>
2026-01-04 19:35:41 -0800

    finished displaying qrcode to open browser and connect in coinase wallet

--- packages/deeplink/src/deeplink.ts
+++ packages/deeplink/src/deeplink.ts
@@ -1,5 +1,5 @@
 
 
 export function buildDeeplinkUrl(): string {
-  return "https://go.cb-w.com/dapp?cb_url=https%3A%2F%2Fsea-pay-ixmsajrvr-thomas-projects-9761171c.vercel.app%2Fpay-mobile"
+  return "https://go.cb-w.com/dapp?cb_url=https%3A%2F%2Fsea-pay-app.vercel.app%2Fpay-mobile"
 }
