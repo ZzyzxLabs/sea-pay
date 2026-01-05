5d6de7d2a229723de3805e4b97bedc164c8ece77
Thomas Lee <thomas930131@gmail.com>
2026-01-04 19:38:36 -0800

    Thomas/base deeplink (#11)
    * add url to open browser in base app
    
    * fix build bug
    
    * remove qr generator dependency
    
    * updated pnpm-lock.yaml
    
    * add next dependency in root package.json
    
    * update deeplink
    
    * update deeplink
    
    * finished displaying qrcode to open browser and connect in coinase wallet

--- packages/deeplink/src/deeplink.ts
+++ packages/deeplink/src/deeplink.ts
@@ -1,5 +1,5 @@
 
 
 export function buildDeeplinkUrl(): string {
-  return "https://go.cb-w.com/dapp?cb_url=https%3A%2F%2Fwww.nike.com%2F"
+  return "https://go.cb-w.com/dapp?cb_url=https%3A%2F%2Fsea-pay-app.vercel.app%2Fpay-mobile"
 }
