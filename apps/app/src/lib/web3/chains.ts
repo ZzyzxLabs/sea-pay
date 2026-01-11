c678c02c11922c1cee3bef0dd8f06df4e09b8324
Thomas Lee <thomas930131@gmail.com>
2026-01-11 12:01:41 -0800

    add chains in wagmi

--- apps/app/src/lib/web3/chains.ts
+++ apps/app/src/lib/web3/chains.ts
@@ -1,10 +1,11 @@
-import { mainnet, base,baseSepolia, polygon, polygonAmoy } from 'wagmi/chains'
+import { mainnet, sepolia, base, baseSepolia, polygon, polygonAmoy } from 'wagmi/chains'
 
-export const chains = [mainnet, base, baseSepolia, polygon, polygonAmoy] as const;
+export const chains = [mainnet, sepolia, base, baseSepolia, polygon, polygonAmoy] as const;
 
 // Use one primary RPC per chain (Alchemy/Infura/QuickNode/etc.)
 export const rpcUrls: Record<number, string> = {
   [mainnet.id]: process.env.NEXT_PUBLIC_MAINNET_RPC_URL!,
+  [sepolia.id]: process.env.NEXT_PUBLIC_SEPOLIA_RPC_URL!,
   [baseSepolia.id]: process.env.NEXT_PUBLIC_BASE_SEPOLIA_RPC_URL!,
   [polygonAmoy.id]: process.env.NEXT_PUBLIC_POLYGON_AMOY_RPC_URL!,
   [polygon.id]: process.env.NEXT_PUBLIC_POLYGON_RPC_URL!,
