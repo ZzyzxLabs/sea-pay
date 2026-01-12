5ea2fa4bee67e7ce9e514e78cbce1c61acf59298
Thomas Lee <thomas930131@gmail.com>
2026-01-12 15:12:36 -0800

    test injected for wagmi

--- apps/app/src/lib/web3/wagmi.ts
+++ apps/app/src/lib/web3/wagmi.ts
@@ -1,6 +1,6 @@
 import { createConfig, http , type Transport } from "wagmi";
 import { chains } from "./chains";
-import { metaMask, coinbaseWallet } from "wagmi/connectors";
+import { metaMask, coinbaseWallet, injected } from "wagmi/connectors";
 
 // const MAINNET_RPC = process.env.NEXT_PUBLIC_MAINNET_RPC_URL!;
 // const SEPOLIA_RPC = process.env.NEXT_PUBLIC_SEPOLIA_RPC_URL!;
@@ -16,17 +16,19 @@ export const config = createConfig({
   chains: chains,
   connectors: [
     // MetaMask SDK connector
-    metaMask({
-      // Optional: only needed if you want the MetaMask SDK itself to do read-only RPC via Infura.
-      // If you already supply your own RPCs in `transports`, you can omit this.
-      infuraAPIKey: process.env.NEXT_PUBLIC_INFURA_API_KEY,
-    }),
+    // metaMask({
+    //   // Optional: only needed if you want the MetaMask SDK itself to do read-only RPC via Infura.
+    //   // If you already supply your own RPCs in `transports`, you can omit this.
+    //   infuraAPIKey: process.env.NEXT_PUBLIC_INFURA_API_KEY,
+    // }),
 
     // Coinbase Wallet connector
     coinbaseWallet({
       appName: "SeaPay",
       appLogoUrl: "https://seapay.ai/logo.png",
     }),
+
+    injected(),
 
   ],
   transports: transports,
